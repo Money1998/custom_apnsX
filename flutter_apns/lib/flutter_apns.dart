830f3b79843596c1280d4229aab510cb674678e4
Money1998 <rahulmakwana.ooo9@gmail.com>
2023-02-13 19:29:29 +0530

    initial commit

--- flutter_apns/lib/flutter_apns.dart
+++
@@ -1,4 +0,0 @@
-export 'package:flutter_apns/src/connector.dart';
-export 'package:flutter_apns/flutter_apns_only_x.dart';
-
-export 'package:firebase_messaging/firebase_messaging.dart' show RemoteMessage;
---
+++ lib/flutter_apns/flutter_apns.dart
@@ -0,0 +1,4 @@
+export 'src/connector.dart';
+export 'flutter_apns_only_x.dart';
+
+export 'package:firebase_messaging/firebase_messaging.dart' show RemoteMessage;
